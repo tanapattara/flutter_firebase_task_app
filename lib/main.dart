e20ec65996984b635d2b839f4fdc294844b56455
Tanapattara Wongkhamchan <tanapattara.w@gmail.com>
2024-09-13 16:46:46 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,170 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+        useMaterial3: true,
+      ),
+      home: const TodaApp(),
+    );
+  }
+}
+
+class TodaApp extends StatefulWidget {
+  const TodaApp({
+    super.key,
+  });
+
+  @override
+  State<TodaApp> createState() => _TodaAppState();
+}
+
+class _TodaAppState extends State<TodaApp> {
+  late TextEditingController _texteditController;
+  late TextEditingController _descriptionController;
+
+  final List<String> _myList = [];
+  @override
+  void initState() {
+    super.initState();
+    _texteditController = TextEditingController();
+    _descriptionController = TextEditingController();
+  }
+
+  void addTodoHandle(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("Add new task"),
+            content: SizedBox(
+              width: 120,
+              height: 140,
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _texteditController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Input your task"),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextField(
+                    controller: _descriptionController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: "Description"),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    CollectionReference Tasks =
+                        FirebaseFirestore.instance.collection("tasks");
+                    Tasks.add({'name': _texteditController.text}).then((res) {
+                      print(res);
+                    }).catchError((onError) {
+                      print("Field to add new Task");
+                    });
+                    setState(() {
+                      _myList.add(_texteditController.text);
+                    });
+                    _texteditController.text = "";
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Save"))
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Todo"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection("tasks").snapshots(),
+          builder: (context, snapshot) {
+            return snapshot.data != null
+                ? ListView.builder(
+                    itemCount: snapshot.data?.docs.length,
+                    itemBuilder: (context, index) {
+                      var item = snapshot.data?.docs[index].data();
+                      return TaskItem(item: item);
+                    })
+                : const Text("No data");
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          addTodoHandle(context);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TaskItem extends StatefulWidget {
+  TaskItem({
+    super.key,
+    required this.item,
+  });
+
+  late Map<String, dynamic>? item;
+
+  @override
+  State<TaskItem> createState() => _TaskItemState();
+}
+
+class _TaskItemState extends State<TaskItem> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(children: [
+        Checkbox(
+            value: widget.item?["status"] ?? false,
+            onChanged: (value) {
+              setState(() {});
+            }),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.item?["name"],
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            if (widget.item?["note"] != null) Text(widget.item?["note"]),
+          ],
+        )
+      ]),
+    );
+  }
+}
